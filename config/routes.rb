cd32d0a35a6786a0cfd89caaa01e06c4c0578ff1
yokoyama takuji <gato.02010417@gmail.com>
2020-09-10 05:45:47 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
 #  root "tasks#index"
   resources :line_bot
   post "/callback" => "line_bot#callback"
-#  get 'tasks/index'
-#  get 'callback/index'
-#  get 'homes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
